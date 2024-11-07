af33353db5a09067efd03e56291c8f2d81901252
Takuma Ikeda <takuma.ikeda@hpe.com>
2024-11-08 01:04:35 +0900

    Update: sharedlib1をbatch1に含める形にワークフローを変更

--- batches/batch1/src/main/java/com/example/HelloWorld.java
+++ batches/batch1/src/main/java/com/example/HelloWorld.java
@@ -3,10 +3,13 @@ package com.example;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
+// import sample.sharedlib.SampleMessage;
 
 public class HelloWorld implements HttpFunction {
     @Override
     public void service(HttpRequest request, HttpResponse response) throws Exception {
+        // SampleMessage message = new SampleMessage("Test Message");
+        // response.getWriter().write("Hello, World!" + message.getMessage());
         response.getWriter().write("Hello, World!");
     }
 }
